41fa4531f9e01ed1463ebe46c60767cfbb70ece6
juan.calvo <juan.calvo@modulrfinance.com>
2019-10-24 13:31:12 +0100

    Wallpaper changer for Windows 10, very useful in certain environments

---
+++ wallpaper.ps1
@@ -0,0 +1,8 @@
+Invoke-WebRequest https://www.tokkoro.com/picsup/2634917-my-little-pony-the-movie-4k-image-for-desktop-wallpaper.jpg -o "$HOME\wallpaper.jpg"
+
+New-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name WallPaper -Value "$HOME\wallpaper.jpg" -PropertyType String -Force | Out-Null
+New-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name WallpaperStyle -Value "3" -PropertyType String -Force | Out-Null
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+sleep 1000
+rm "$HOME\wallpaper.jpg"
+exit
