08034d4a4afb8fc601627ea57830855aab0c27a8
juan.calvo <juan.calvo@modulrfinance.com>
2019-10-24 14:08:54 +0100

    Windows doing windows

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -3,6 +3,9 @@ Invoke-WebRequest https://www.tokkoro.com/picsup/2634917-my-little-pony-the-movi
 New-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name WallPaper -Value "$HOME\wallpaper.jpg" -PropertyType String -Force | Out-Null
 New-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name WallpaperStyle -Value "3" -PropertyType String -Force | Out-Null
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-sleep 1000
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+sleep 1
 rm "$HOME\wallpaper.jpg"
 exit
